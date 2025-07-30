41cd60ed72a7859dec2f5dec2123f7ea0f4b26d2
vulpes-a <vulpesily@gmail.com>
2025-07-30 10:39:45 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -8,25 +8,49 @@ int main() {
     int torre;
 
     // Movimentação do Bispo
+
+    printf("--Bispo--\n");
+
     while (bispo < 5) {
         
-        printf("Bispo: Cima Direita \n");
+        printf("Cima Direita \n");
 
         bispo++;
     }
+    printf("\n");
 
     //Movimentação da Torre
+
+   printf("--Torre--\n");
+
     for (torre = 0; torre < 5; torre++) {
-        printf("Torre : Direita \n");
+        printf("Direita \n");
     }
+    printf("\n");
 
     //Movimentação da Rainha
+
+    printf("--Rainha--\n");
+
     do {
 
-        printf("Rainha: Esquerda \n");
+        printf("Esquerda \n");
         rainha++;
         
     } while (rainha < 8);
+    printf("\n");
+
+    //Movimentação do Cavalo
+
+    printf("--Cavalo--\n");
+    for (int cavalo = 0; cavalo <= 2; cavalo++) {
+        while (cavalo < 2) {
+            printf("Baixo, ");
+            cavalo++;
+        }
+        printf("Esquerda \n");
+    }
+    printf("\n");
 
     return 0;
 }
